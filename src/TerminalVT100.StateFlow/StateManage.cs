370ec30f9e355e067d001a0cf170bd019ade81e3
Rodrigo Baia <rodrigo@novasistemas.com.br>
2024-06-20 23:00:48 -0300

    Alterado o nome da classe

---
+++ src/TerminalVT100.StateFlow/StateManage.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalVT100.StateFlow
+{
+    public class StateManage
+    {
+        private TedVT100Server TedVT100Server;
+        private ConcurrentDictionary<string, ClientState> _clientStates;
+
+        public static ILogger Logger;
+
+        public StateManage()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var pathLog = Path.Combine(path, "logs");
+
+            if (!Directory.Exists(pathLog))
+            {
+                Directory.CreateDirectory(pathLog);
+            }
+
+            var fileFull = Path.Combine(pathLog, "state-manage.log");
+
+            Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(fileFull, rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+#if DEBUG
+                .WriteTo.Console(Serilog.Events.LogEventLevel.Verbose)
+#else
+                .WriteTo.Console(Serilog.Events.LogEventLevel.Error)
+#endif
+                .CreateLogger();
+        }
+
+
+
+    }
+}
